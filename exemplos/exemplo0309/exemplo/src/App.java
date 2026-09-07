dd5acd8b02795205b6127f27ec8dd32882854114
Rafael <130803096+rafael019wi@users.noreply.github.com>
2026-09-07 12:06:23 -0300

    Commit exemplos strings

---
+++ exemplos/exemplo0309/exemplo/src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador();
+        jogador1.nome = "A";
+        System.out.println(jogador1.nome);
+    }
+}
---
+++ exemplos/exemplo0309/exemplo/src/Jogador.java
@@ -0,0 +1,12 @@
+public class Jogador {
+     String nome;
+     int NumeroVidas;
+     int destreza;
+
+    // public Jogador(String nome,int v, int d){
+    //     this.nome = nome;
+    //     this.NumeroVidas = v;
+    //     this.destreza = d;
+    // }
+
+}
